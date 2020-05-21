205f84bd9266dbb19cddbc4702b3a4569f4e8597
Decu98 <decuux@gmail.com>
2020-05-22 01:36:46 +0200

    Dodanie lokalnej pamięci do zbioru ksiazek

--- Biblioteka/scripts.js
+++ Biblioteka/scripts.js
@@ -1,21 +1,22 @@
 
-
-var biblioteka = [];
-var IdOfBook = 1;
+var biblioteka = []
 function _addbook(){
-	var currentlibrery = sessionStorage.getItem('biblioteka')
-	biblioteka = currentlibrery;
+	var loadData = localStorage.getItem('books');
+	console.log(loadData);
+	if(loadData !== null){
+		biblioteka = JSON.parse(loadData);
+	}
 	var tytul = document.getElementById('Tytul').value;
 	var autor = document.getElementById('Autor').value;
 	var isbn = document.getElementById('ISBN').value;
 	var ilosc = document.getElementById('Ilosc').value;
 	if(tytul.length == 0 || autor.length == 0 || isbn.length == 0 || ilosc.length == 0){
 		alert("Uzupełni dane");
-	}
-	var ksiazka = {Tytuł: tytul, Autor: autor, ISBN: isbn, Ilość: ilosc, ID: IdOfBook};
-	biblioteka.push(ksiazka);
-	IdOfBook = IdOfBook + 1;
-	sessionStorage.setItem('biblioteka','biblioteka');
-	console.log("Działa");
-	console.log(biblioteka);
+	}else{
+		var ksiazka = {Tytuł: tytul, Autor: autor, ISBN: isbn, Ilość: ilosc};
+		biblioteka.push(ksiazka)
+		localStorage.setItem('books', JSON.stringify(biblioteka));
+		console.log(localStorage);
+	
+	}	
 }
