32c0553a2927027e0b50c42350a281199d617b08
Decu98 <decuux@gmail.com>
2020-05-24 19:59:10 +0200

    Wyswietlanie ksiazek w bibliotece

--- Biblioteka/scripts.js
+++ Biblioteka/scripts.js
@@ -1,11 +1,7 @@
 
 var biblioteka = []
 function _addbook(){
-	var loadData = localStorage.getItem('books');
-	console.log(loadData);
-	if(loadData !== null){
-		biblioteka = JSON.parse(loadData);
-	}
+	libraryUpdate();
 	var tytul = document.getElementById('Tytul').value;
 	var autor = document.getElementById('Autor').value;
 	var isbn = document.getElementById('ISBN').value;
@@ -16,7 +12,27 @@ function _addbook(){
 		var ksiazka = {Tytuł: tytul, Autor: autor, ISBN: isbn, Ilość: ilosc};
 		biblioteka.push(ksiazka)
 		localStorage.setItem('books', JSON.stringify(biblioteka));
-		console.log(localStorage);
 	
 	}	
+}
+
+function toLibrary(){
+	libraryUpdate();
+	for(i = 0; i < biblioteka.length; i++){
+		var n1 = biblioteka[i].Tytuł;
+		var n2 = biblioteka[i].Autor
+		var n3 = biblioteka[i].ISBN
+		var whole = "Tytuł: " + n1 + " " + "Autor: " + n2 + " " + "ISBN: " + n3;
+		var newDiv = document.createElement("H1");
+		var textnode = document.createTextNode(whole);
+		var position = document.getElementById("Main")
+		newDiv.appendChild(textnode);
+		position.appendChild(newDiv);
+	}
+}
+function libraryUpdate(){
+	var loadData = localStorage.getItem('books');
+	if(loadData !== null){
+		biblioteka = JSON.parse(loadData);
+	}
 }
