3c46b24cef2aa9fc5f2c5e23e2f8d8f9deaf5ef6
Decu98 <decuux@gmail.com>
2020-05-25 07:19:54 +0200

    Dodatnie opcji edycji i wypozyczenia ksiazki

--- Biblioteka/scripts.js
+++ Biblioteka/scripts.js
@@ -23,11 +23,22 @@ function toLibrary(){
 		var n2 = biblioteka[i].Autor
 		var n3 = biblioteka[i].ISBN
 		var whole = "Tytuł: " + n1 + " " + "Autor: " + n2 + " " + "ISBN: " + n3;
-		var newDiv = document.createElement("H1");
+		var newH = document.createElement("P");
+		newH.setAttribute("id", "H1_" + i);
+		var newB0 = document.createElement("button");
+		var newB1 = document.createElement("button");
+		newB0.setAttribute("id", "Button0_" + i);
+		newB1.setAttribute("id", "Button1_" + i);
+		newB0.setAttribute("onclick", "window.location.href = 'editbook.html'; saveEdit(this.id)")
+		newB1.setAttribute("onclick", "window.location.href = 'rent.html'; saveEdit(this.id)")
+		newB0.innerHTML = "Edytuj";
+		newB1.innerHTML = "Wypożycz";
 		var textnode = document.createTextNode(whole);
-		var position = document.getElementById("Main")
-		newDiv.appendChild(textnode);
-		position.appendChild(newDiv);
+		var position = document.getElementById("Main");
+		newH.appendChild(textnode);
+		newH.appendChild(newB0);
+		newH.appendChild(newB1);
+		position.appendChild(newH);
 	}
 }
 function libraryUpdate(){
@@ -35,4 +46,12 @@ function libraryUpdate(){
 	if(loadData !== null){
 		biblioteka = JSON.parse(loadData);
 	}
+}
+
+function saveEdit(x){
+	localStorage.setItem("IDofBookEdited", x);
+}
+
+function approveEdit(){
+	localStorage.removeItem("IDofBookEdited");
 }
