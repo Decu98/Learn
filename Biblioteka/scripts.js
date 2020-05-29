327369558d65d44cfc35fa07c9ad0c613c1bdb61
Decu98 <decuux@gmail.com>
2020-05-29 16:37:15 +0200

    Naprawa wyswietlania przy wypozyczeniu

--- Biblioteka/scripts.js
+++ Biblioteka/scripts.js
@@ -9,34 +9,6 @@ function libraryUpdate(){
 	}
 }
 
-function showResults(x){
-	for(i = 0; i < x.length; i++){
-		var book = x[i];
-		var newDivElement = document.createElement("div");
-		newDivElement.setAttribute("class", "LibraryBooks");
-		var editButton = document.createElement("button");
-		var rentButton = document.createElement("button");
-		editButton.setAttribute("id", "Button0_" + book.ID_);
-		rentButton.setAttribute("id", "Button1_" + book.ID_);
-		delete book.Ilość;
-		for(var key in book){
-			var newX = document.createElement("P");
-			var textnode = document.createTextNode(key + ": " + book[key]);
-			newX.appendChild(textnode);
-			newDivElement.appendChild(newX);
-		}
-
-		editButton.setAttribute("onclick", `window.location.href = "editbook.html?bookCode=${book.ID_}"`);
-		rentButton.setAttribute("onclick", `window.location.href = "rent.html?bookCode=${book.ID_}"`)
-		editButton.innerHTML = "Edytuj";
-		rentButton.innerHTML = "Wypożycz";
-		var position = document.getElementById("Main");
-		newDivElement.appendChild(editButton);
-		newDivElement.appendChild(rentButton);
-		position.appendChild(newDivElement);
-	}
-}
-
 function clearID(){
 	localStorage.removeItem("IDofBookEdited");
 }
