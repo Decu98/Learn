00a70ffc1df260099446b9e3e1e8beb92a04935e
Decu98 <decuux@gmail.com>
2020-06-05 01:16:51 +0200

    Usuniecie zbednego pobierania listy

--- Biblioteka/editElement.js
+++ Biblioteka/editElement.js
@@ -3,30 +3,26 @@ function editElement(element) {
     switch (element) {
         case "book":
             updateStorage("book");
-            var getBooks = localStorage.getItem("books");
             var bookID = new URLSearchParams(window.location.search).get('bookCode');
             var newTitle = document.getElementById("TitleEdit").value;
             var newAuthor = document.getElementById("AuthorEdit").value;
             var newCount = document.getElementById("CountEdit").value;
-            var temporarySave = JSON.parse(getBooks);
-            temporarySave[bookID].Tytuł = newTitle;
-            temporarySave[bookID].Autor = newAuthor;
-            temporarySave[bookID].Ilosc = newCount;
-            localStorage.setItem('books', JSON.stringify(temporarySave));
+            library[bookID].Tytuł = newTitle;
+            library[bookID].Autor = newAuthor;
+            library[bookID].Ilosc = newCount;
+            localStorage.setItem('books', JSON.stringify(library));
             alert("Zedytowano książkę");
             break;
         case "user":
             updateStorage("user");
-            var getUser = localStorage.getItem("users");
             var userId = new URLSearchParams(window.location.search).get('userCode');
             var newName = document.getElementById("editName").value;
             var newSurname = document.getElementById("editSurname").value;
             var newPESEL = document.getElementById("editPESEL").value;
-            var temporarySave = JSON.parse(getUser);
-            temporarySave[userId].Imie = newName;
-            temporarySave[userId].Nazwisko = newSurname;
-            temporarySave[userId].PESEL = newPESEL;
-            localStorage.setItem('users', JSON.stringify(temporarySave));
+            library[userId].Imie = newName;
+            library[userId].Nazwisko = newSurname;
+            library[userId].PESEL = newPESEL;
+            localStorage.setItem('users', JSON.stringify(library));
             alert("Zedytowano użytkownika");
             break;
         default:
